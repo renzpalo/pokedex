b408826c939eb471f52a7abdc490bc9ef44f2a3c
Renz Palo <renzpalo@gmail.com>
2020-05-07 00:48:14 -0700

    added dev dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -13,13 +13,21 @@ module.exports = {
         rules: [
             {
                 test: /\.(scss)$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader'
-                ] 
-            }
+                use: [{
+                    loader: 'style-loader', // inject CSS to page
+                }, {
+                    loader: 'css-loader', // translates CSS into CommonJS modules
+                }, {
+                    loader: 'postcss-loader', // Run postcss actions
+                    options: {
+                    plugins: function () { // postcss plugins, can be exported to postcss.config.js
+                        return [require('autoprefixer')];
+                    }
+                    }
+                }, {
+                    loader: 'sass-loader' // compiles Sass to CSS
+                }]
+            },
         ]
     }
 }
